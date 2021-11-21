cb675756c3dd85cd2d8187f9b01ea119102459b5
Алексей Шиян <alex-shiyan@yandex.ru>
2021-11-22 01:31:17 +0300

    Домашняя работа по заданию 6

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿//
+// Домашнее задание к занятию 6. Строки
+//
+// Задача 1. Ввести с клавиатуры предложение.
+// Предложение представляет собой слова, разделенные пробелом.
+// Знаки препинания не используются.
+// Найти самое длинное слово в строке
+//
+Console.WriteLine();
+// Вводим фразу с клавиатуры
+string stringStart = Console.ReadLine();
+Console.WriteLine();
+// Разбиваем фразу на слова
+string[] stingArray = stringStart.Split();
+// Считаем количество слов в предложении
+int sumWord = 0;
+foreach (string s in stingArray)
+{
+        sumWord++;
+}
+Console.WriteLine("Количество слов в предложении равно {0}",sumWord);
+// Создаем массив из длин слов и находим самое длинное слово
+int[] arrayLength = new int [sumWord];
+int max = 0;
+foreach (string s in stingArray)
+{
+    int n = Convert.ToInt32(s.Length);
+    if (n > max)
+        max = n;
+}
+Console.WriteLine("Количество знаков в самом длинном слове равно {0}",max);
+Console.WriteLine();
+// Выводим саме длинные слова
+Console.Write("Самое длинное слово: ");
+int kolWord = 0;
+foreach (string s in stingArray)
+{
+    int n = Convert.ToInt32(s.Length);
+        if (n == max)
+        {
+          kolWord++;
+          Console.Write("{0} ",s);
+        }
+}
+Console.WriteLine();
+// Завершение программы
+Console.Write("Для завершения программы нажмите любую клавишу");
+Console.ReadKey();
