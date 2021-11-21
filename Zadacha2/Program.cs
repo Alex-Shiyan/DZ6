88479d779d4c50dc07b5cbb7f958a203e5dd1611
Alex-Shiyan <94722049+Alex-Shiyan@users.noreply.github.com>
2021-11-22 01:49:15 +0300

    Add files via upload

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,55 @@
+﻿//
+// Домашнее задание к занятию 6. Строки
+//
+// Задача 2. Ввести с клавиатуры предложение.
+// Предложение представляет собой слова, разделенные пробелом.
+// Знаки препинания не используются.
+// Составить программу, определяющую является ли строка палиндромом
+// без учёта пробелов и регистра
+// (пример палиндрома – «А роза упала на лапу Азора»).
+//
+Console.WriteLine();
+// Вводим фразу с клавиатуры
+string stringStart = Console.ReadLine();
+// Понижаем регистр
+stringStart = stringStart.ToLower();
+// Разбиваем фразу на слова
+string[] stingArray = stringStart.Split();
+// Сшиваем слова в фразу для удаление пробелов
+string bigString = "";
+string probel = " ";
+foreach (string s in stingArray)
+{
+    if (s != probel)
+    {
+        bigString += s;
+    }
+}                                                                                                      
+// Разбиваем суперслово на буквы и переворачиваем
+char[] charArray = bigString.ToArray();
+char[] antiArray = new char[charArray.Length];
+int n = Convert.ToInt32(bigString.Length);
+for (int i = 0; i < n; i++)
+{
+    int j = n - i - 1;
+    antiArray[i] = charArray[j];
+}
+Console.WriteLine();
+// сравниваем два слова
+for (int i = 0; i < n; i++)
+{
+    if (charArray[i] != antiArray[i])
+    {
+        Console.WriteLine("Это не палиндром");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Это палиндром");
+        break;
+    }
+}
+Console.WriteLine();
+// Завершение программы
+Console.Write("Для завершения программы нажмите любую клавишу");
+Console.ReadKey();
---
+++ Zadacha3/Program.cs
@@ -0,0 +1,50 @@
+﻿//
+// Домашнее задание к занятию 6. Строки
+//
+// Задача 3. Дана строка S. Из строки требуется удалить текст, заключенный в фигурные скобки.
+// В строке может быть несколько фрагментов, заключённых в фигурные скобки.
+// Возможно использование вложенных фигурных скобок, необходимо, чтобы программа это учитывала.
+//
+Console.WriteLine();
+//Вводим фразу с клавиатуры
+//string stringS = Console.ReadLine();
+// Задаем тестовую фразу
+Console.WriteLine("Исходная фраза:");
+Console.WriteLine();
+string stringS = "Дана строка S. Из строки требуется удалить текст, {заключенный в фигурные скобки. {В строке может быть несколько фрагментов, заключённых в фигурные скобки.} Возможно {использование вложенных фигурных скобок}, необходимо,} чтобы программа это учитывала.";
+Console.WriteLine(stringS);
+// Определим длину фразы
+int n = Convert.ToInt32(stringS.Length);
+// Ищем номер самой левой скобки
+int numberL = 0;
+for (int i = 0; i < n; i++)
+{
+    if (stringS[i] == '{')
+    {
+        numberL = i;
+        break;
+    }
+}
+//Console.WriteLine("Номер самой левой скобки = {0}", numberL);
+// Ищем номер самой правой скобки
+int numberR = 0;
+for (int i = 0; i < n; i++)
+{
+    if (stringS[i] == '}')
+    {
+        numberR = i;
+    }
+}
+//Console.WriteLine("Номер самой правой скобки = {0}", numberR);
+int deltaD = numberR - numberL;
+//Console.WriteLine("Расстояние между символами рано {0}", deltaD);
+stringS = stringS.Remove(numberL-1, deltaD+2);
+Console.WriteLine();
+Console.WriteLine("Итоговая фраза:  ");
+Console.WriteLine();
+Console.WriteLine(stringS);
+Console.WriteLine();
+Console.WriteLine();
+// Завершение программы
+Console.Write("Для завершения программы нажмите любую клавишу");
+Console.ReadKey();
